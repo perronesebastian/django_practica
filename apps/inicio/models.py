4888610b4745313b3010c812c03596c5e99a3108
perronesebastian <perronesebastian@hotmail.com>
2021-12-13 19:37:19 -0300

    model empleado y departamento

--- apps/inicio/models.py
+++ apps/inicio/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.db.models.deletion import SET_DEFAULT, SET_NULL
 
 # Create your models here.
+
+class Departamento(models.Model):
+    nombre = models.CharField(max_length=50, null = False, blank = False)
+    fecha_creacion = models.DateTimeField(auto_now_add = True)
+    fecha_modificacion = models.DateTimeField(auto_now = True)
+
+class Empleado(models.Model):
+    nombre = models.CharField(max_length=50, null=False, blank=False)
+    apellido = models.CharField(max_length=50, null=False, blank=False)
+    fecha_nacimiento = models.DateTimeField()
+    sueldo = models.DecimalField(max_digits = 10, decimal_places=2, default=10000)
+    departamento = models.ForeignKey(Departamento, on_delete=SET_NULL, null=True)
